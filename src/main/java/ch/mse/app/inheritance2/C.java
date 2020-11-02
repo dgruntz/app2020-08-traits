86065b2fb59354e234c1f37008ad5538bb983cbe
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2020-11-02 10:55:08 +0100

    adds multiple-inheritance pseudo code

--- src/main/java/ch/mse/app/inheritance2/C.java
+++ src/main/java/ch/mse/app/inheritance2/C.java
@@ -1,5 +1,25 @@
 package ch.mse.app.inheritance2;
 
+// public class C extens A, B {
+// 	@Override
+// 	public void foo() {
+// 		System.out.println("C::foo");
+// 		super.foo();
+// 	}
+//
+// 	@Override
+// 	public void bar() {
+// 		System.out.println("C::bar");
+// 		test();
+// 	}
+//
+// 	@Override
+// 	public void test() {
+// 		System.out.println("C::test");
+// 		super.test();
+// 	}
+// }
+
 public class C extends B implements IA {
 	final _A _super;
 
